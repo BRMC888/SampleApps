4c86b90c5713a71f16fa481ac36f696f28769d46
Joe <wearing.joe@gmail.com>
2019-09-02 12:08:53 +0100

    First commit

---
+++ PHPApp/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Garage Blog</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="p-5">
+            <h1>Welcome to Garage Blog</h1>
+
+            <p class="mt-4">Feel free to take a look around</p>
+
+            <p><?php echo "Also, if you're reading this line, php has been successfully executed" ?></p>
+        </div>
+    </body>
+</html>
